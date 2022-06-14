772ce28df50470acc0dac8462931b34227e577d2
Tomas Civini <tcivini@gmail.com>
2022-06-14 12:26:29 -0300

    now options is not displayed in searchbar until input

--- src/components/common/AutoComplete.tsx
+++ src/components/common/AutoComplete.tsx
@@ -7,22 +7,27 @@ type BarProps = {
   options?: Options[];
 };
 
-const PopperMy = function (props: any) {
-  return (<Popper {...props} style={{ width: 25000, maxHeight: '50%' }} placement='bottom-start' />)
-}
-
 export default function AutoComplete({ options }: BarProps) {
+
+  const [open, setOpen] = React.useState(false);
+
   return (
-    <div style={{display: 'flex', alignItems: 'flex-start', width: '100%', columnGap: 1000}}>
     <Autocomplete
       id="combo-box-demo"
       options={options!}
       getOptionLabel={(option) => option.id + " - " + option.name}
-      sx={{}}
-      style={{}}
       fullWidth
       renderInput={(params) => <TextField {...params} label="Proyecto" sx={{}}/>}
+      freeSolo
+      open={open}
+      onInputChange={(_, value) => {
+        if (value.length === 0) {
+          if (open) setOpen(false);
+        } else {
+          if (!open) setOpen(true);
+        }
+      }}
+      onClose={() => setOpen(false)}
     />
-    </div>
   );
 }
