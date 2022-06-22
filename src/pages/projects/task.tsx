1e5ddce4fc6e3909394e6d4cae8f5b48bc278c2f
Tomas Civini <tcivini@gmail.com>
2022-06-22 15:24:33 -0300

    added mock list of colaborators to tasks

--- src/pages/projects/task.tsx
+++ src/pages/projects/task.tsx
@@ -1,18 +1,28 @@
 import type { NextPage } from "next";
 import { useState } from "react";
-import { Box, Typography, Button } from "@material-ui/core";
+import { Box, Typography, Button, ListItem, ListItemText, IconButton, ListItemIcon } from "@material-ui/core";
 import { useTask } from "../../services/projects";
 import { useRouter } from "next/router";
 import { zeroPad } from "../../util/util";
 import Loading from "../../components/common/Loading";
 import TaskModal from "../../components/projects/tasks/TaskModal";
 import TitledText from "../../components/common/TitledText";
+import CloseIcon from '@mui/icons-material/Close';
+const colabs: string[] = [
+  "hola",
+  "german",
+  "asdasdasdasdasdasdasdaszd"
+]
 
 const Task: NextPage = () => {
   const router = useRouter();
   const taskId = router?.query?.id as string;
   const { task, error, loading, mutate } = useTask(taskId);
   const [open, setOpen] = useState(false);
+
+  const onHours = () => {
+    console.log("cargar horas");
+  }
 
   return (
     <div className="page">
@@ -39,12 +49,46 @@ const Task: NextPage = () => {
               Editar Tarea
             </Button>
           </Box>
-          <TitledText title="Fecha de inicio">{task?.initial_date}</TitledText>
-          <TitledText title="Fecha de fin">{task?.final_date}</TitledText>
-          <TitledText title="Horas estimadas">
-            {task?.estimated_hours}
-          </TitledText>
-          <TitledText title="Descripción">{task?.description}</TitledText>
+          <div>
+          <div>
+            <TitledText title="Fecha de inicio">{task?.initial_date}</TitledText>
+            <TitledText title="Fecha de fin">{task?.final_date}</TitledText>
+            <TitledText title="Horas estimadas">
+              {task?.estimated_hours}
+            </TitledText>
+            <TitledText title="Descripción">{task?.description}</TitledText>
+          </div>
+          <div style={{width: "600px", padding: 2, marginTop: 20, marginBottom: 20}}>
+            <>
+            <Typography variant="overline" style={{ lineHeight: "normal" }}>
+              Colaboradores
+            </Typography>
+            {colabs?.map((colab: string) => (
+              <ListItem
+              key={colab}
+              divider
+            >
+              <ListItemText primary={colab} primaryTypographyProps={{ style: { whiteSpace: "normal" } }} style={{}}/>
+              <ListItemIcon>
+                <IconButton aria-label="deleteColaborator">
+                  <CloseIcon/>
+                </IconButton>
+              </ListItemIcon>
+            </ListItem>
+            ))}
+            </>
+          </div>
+          <div>
+          <Button
+              variant="contained"
+              color="primary"
+              style={{ height: "fit-content" }}
+              onClick={onHours}
+            >
+              Cargar horas
+            </Button>
+          </div>
+          </div>
           <TaskModal
             open={open}
             onClose={() => setOpen(false)}
