b00638ea2669368fa1471b918ac6c6ffd79678c8
fdelu <fdeluca@fi.uba.ar>
2022-06-22 00:07:06 -0300

    Add better error messages

--- src/components/projects/ProjectsList.tsx
+++ src/components/projects/ProjectsList.tsx
@@ -5,6 +5,7 @@ import Loading from "../common/Loading";
 import { Typography, Box } from "@material-ui/core";
 import Caption from "../common/Caption";
 import { pluralize } from "../../util/util";
+import Alert from "@mui/material/Alert";
 
 type ProjectsListProps = {
   projects: Project[];
@@ -15,8 +16,12 @@ type ProjectsListProps = {
 const ProjectsList = ({ projects, error, loading }: ProjectsListProps) => {
   return (
     <div className={styles.ProjectsList + " flexContainer"}>
-      {loading ? <Loading /> : ""}
-      {error ? "ERROR" : ""}
+      {loading && !error ? <Loading /> : ""}
+      {error ? (
+        <Alert severity="error" style={{ width: "100%" }}>
+          No se pudieron cargar los proyectos
+        </Alert>
+      ) : null}
       {projects?.map((project: Project, i: number) => (
         <InfoCard key={i} info={project} link="/projects/project?id=">
           <Box
@@ -33,7 +38,7 @@ const ProjectsList = ({ projects, error, loading }: ProjectsListProps) => {
           </Box>
         </InfoCard>
       ))}
-      <Caption>{projects?.length ? "" : "No hay proyectos"}</Caption>
+      <Caption>{projects?.length === 0 ? "No hay proyectos" : ""}</Caption>
     </div>
   );
 };
--- src/components/projects/tasks/TasksList.tsx
+++ src/components/projects/tasks/TasksList.tsx
@@ -2,6 +2,7 @@ import { Task } from "../../../services/types";
 import InfoCard from "../../common/Card";
 import Loading from "../../common/Loading";
 import Caption from "../../common/Caption";
+import Alert from "@mui/material/Alert";
 import styles from "../Projects.module.css";
 
 type TasksListProps = {
@@ -13,12 +14,16 @@ type TasksListProps = {
 const TasksList = ({ tasks, error, loading }: TasksListProps) => {
   return (
     <div className={styles.TasksList + " flexContainer"}>
-      {loading ? <Loading /> : ""}
-      {error ? "ERROR" : ""}
+      {loading && !error ? <Loading /> : ""}
+      {error ? (
+        <Alert severity="error" style={{ width: "100%" }}>
+          No se pudieron cargar las tareas
+        </Alert>
+      ) : null}
       {tasks?.map((task: Task, i: number) => (
         <InfoCard key={i} info={task} link="/projects/task?id=" />
       ))}
-      <Caption>{tasks?.length ? "" : "No hay tareas"}</Caption>
+      <Caption>{tasks?.length === 0 ? "No hay tareas" : ""}</Caption>
     </div>
   );
 };
