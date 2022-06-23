a3eb551db7ae6a5f7d869a15270b0e1b9a63c0dc
Tomas Civini <tcivini@gmail.com>
2022-06-23 17:02:10 -0300

    changed project resource api to https

--- src/services/requests.ts
+++ src/services/requests.ts
@@ -2,4 +2,4 @@ export { default as useSWR } from "swr";
 
 export const projectsApi = "https://aninfo-projects.herokuapp.com/api/v1";
 
-export const projectsResourcesApi = "http://aninfo-resources-external.herokuapp.com";
+export const projectsResourcesApi = "https://aninfo-resources-external.herokuapp.com";
