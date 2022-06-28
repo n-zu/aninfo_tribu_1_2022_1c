723032a3e5584d2bd6ac9ab7dcabfe046cefaa3b
LucasWaisten <waistenlucas@gmail.com>
2022-06-28 17:42:06 -0300

    Pages Recursos Humanos

---
+++ src/pages/rrhh/registrohoras.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import { useRecursos } from "../../services/rrhh";
+
+
+const RegistroHoras: NextPage = () => {
+    const recursos = useRecursos();
+  return (
+    <div className="page">
+        hola
+    </div>
+  );
+};
+
+export default RegistroHoras;
