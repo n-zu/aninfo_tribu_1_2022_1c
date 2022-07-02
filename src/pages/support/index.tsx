980b08c525334c19829afc35b553e531a930a9c5
josuebouchard <7890663+josuebouchard@users.noreply.github.com>
2022-07-02 19:26:44 -0300

    merged some files

--- src/pages/support/index.tsx
+++ src/pages/support/index.tsx
@@ -6,38 +6,38 @@ import useSWR from "swr";
 const supportFetcher = (resource: string) => fetch('https://squad320221c-production.up.railway.app' + resource).then(res => res.json())
 
 type ProjectVersion = {
-  id: number,
-  name: string
+    id: number,
+    name: string
 }
 
 type Project = {
-  id: number,
-  name: string,
-  versions: ProjectVersion[]
+    id: number,
+    name: string,
+    versions: ProjectVersion[]
 }
 
 const SupportProject: NextPage = () => {
-  const { data, error } = useSWR<Project[]>('/products', supportFetcher);
+    const { data, error } = useSWR<Project[]>('/products', supportFetcher);
 
-  return <Container className="page">
-    <h1>Proyectos</h1>
+    return <Container className="page">
+        <h1>Proyectos</h1>
 
-    {data?.map(product => (
-      <Card key={product.id} style={{ marginTop: "1em" }}>
-        <CardContent>
-          <Typography variant="h6">{product.name}</Typography>
-        </CardContent>
-        <CardActions>
-          {product.versions.map(version => (
-            <Link key={version.id} href={`/support/${version.id}/tickets`}>
-              <Button>{version.name}</Button>
-            </Link>
-          ))}
-        </CardActions>
-      </Card>
-    ))}
+        {data?.map(product => (
+            <Card key={product.id} style={{ marginTop: "1em" }}>
+                <CardContent>
+                    <Typography variant="h6">{product.name}</Typography>
+                </CardContent>
+                <CardActions>
+                    {product.versions.map(version => (
+                        <Link key={version.id} href={`/support/${version.id}/tickets`}>
+                            <Button>{version.name}</Button>
+                        </Link>
+                    ))}
+                </CardActions>
+            </Card>
+        ))}
 
-  </Container>
+    </Container>
 }
 
 export default SupportProject;
